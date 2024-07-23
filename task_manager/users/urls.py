9307b773d5b9a3def5ef9448b25752aebdd38b14
Sergey R <topgoods687@gmail.com>
2024-07-23 14:07:16 +0300

    add users index route template view

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -3,5 +3,5 @@ from task_manager.users import views
 
 
 urlpatterns = [
-    path('', views.UserIndexView.as_view(), name='user_index'),
+    path('', views.UserIndexView.as_view(), name='users_index'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -6,3 +6,4 @@ from django.contrib.auth.models import User
 class UserIndexView(ListView):
     model = User
     template_name = 'users/index.html'
+    context_object_name = 'users'
