cdcc373a6739a9085ae480568c20fafe680e9363
Sergey R <topgoods687@gmail.com>
2024-07-23 12:55:24 +0300

    change allowed_hosts settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,11 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = [
-    'webserver',
-    '127.0.0.1',
-    'localhost',
-]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
