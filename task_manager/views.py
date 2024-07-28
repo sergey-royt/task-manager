20912b64ea7e2d68433a83f8dc9c06bd563ebb8e
Sergey R <topgoods687@gmail.com>
2024-07-28 12:45:21 +0300

    add all navbar items and messages for login and logout

--- task_manager/views.py
+++ task_manager/views.py
@@ -4,19 +4,25 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import AuthenticationForm
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-class UserLoginView(LoginView, SuccessMessageMixin):
+class UserLoginView(SuccessMessageMixin, LoginView):
     template_name = 'form.html'
     authentication_form = AuthenticationForm
     next_page = reverse_lazy('index')
     extra_context = {'title': _('Authorization'),
                      'button_text': _('Login')}
+
     success_message = _('You are Logged in')
 
-class UserLogoutView(LogoutView, ):
+class UserLogoutView(LogoutView):
     next_page = reverse_lazy('index')
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.add_message(request, messages.INFO, _('You are logged out'))
+        return super().dispatch(request, *args, **kwargs)
