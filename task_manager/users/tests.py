c728097bfe22086f413ece5a0e31ccce1a7d7de5
Sergey R <topgoods687@gmail.com>
2024-07-29 17:27:57 +0300

    add test for user update view

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -32,3 +32,65 @@ class UsersCreateTest(TestCase):
         user = User.objects.last()
         self.assertEqual(user.username, username)
         user.delete()
+
+
+class UserUpdateTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        faker = Faker()
+
+        username_1 = faker.user_name()
+        password_1 = faker.password(length=10)
+        username_2 = faker.user_name()
+        password_2 = faker.password(length=10)
+        self.authorized_user = User.objects.create_user(username=username_1,
+                                                        password=password_1)
+        self.unauthorized_user = User.objects.create_user(username=username_2,
+                                                          password=password_2)
+        self.authorized_user.save()
+        self.unauthorized_user.save()
+
+        self.client.force_login(self.authorized_user)
+
+        self.new_username = faker.user_name()
+        self.new_user_data = {'username': self.new_username}
+
+    def tearDown(self):
+        self.authorized_user.delete()
+        self.unauthorized_user.delete()
+
+    def test(self):
+        self._test_update_user_allowed_get()
+        self._test_update_user_restricted_get()
+        self._test_update_user_allowed_post()
+        self._test_update_user_restricted_post()
+
+    def _test_update_user_allowed_get(self):
+        response = self.client.get(
+            reverse('user_update', kwargs={'id': self.authorized_user.id})
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def _test_update_user_restricted_get(self):
+        response = self.client.get(
+            reverse('user_update', kwargs={'id': self.unauthorized_user.id})
+        )
+
+        self.assertFalse(response.status_code == HTTPStatus.OK)
+
+    def _test_update_user_allowed_post(self):
+        response = self.client.post(
+            reverse('user_update', kwargs={'id': self.authorized_user.id}),
+            data=self.new_user_data
+        )
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(self.authorized_user.username, self.new_username)
+
+    def _test_update_user_restricted_post(self):
+        response = self.client.post(
+            reverse('user_update', kwargs={'id': self.unauthorized_user.id}),
+            data=self.new_user_data
+        )
+        self.assertFalse(response.status_code == HTTPStatus.FOUND)
+        self.assertFalse(self.authorized_user.username == self.new_username)
