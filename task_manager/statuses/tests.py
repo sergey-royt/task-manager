e44772f23f0f7d7b114922aca285cd4adc6f3ee2
Sergey R <topgoods687@gmail.com>
2024-08-12 14:20:57 +0300

    add users fixture

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from faker import Faker
+from http import HTTPStatus
 
-# Create your tests here.
+
+class TestStatus(TestCase):
+    fixtures = ['users.json']
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        ...
