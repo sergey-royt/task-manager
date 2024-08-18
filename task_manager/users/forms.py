bc4675429644c695fc08b53346eb4c6c8f1f86c5
Sergey R <topgoods687@gmail.com>
2024-08-18 16:02:40 +0300

    refactor user update form add ability to not to change username while updating

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+
 from .models import CustomUser
 from django.contrib.auth.forms import UserCreationForm
 
@@ -12,5 +14,29 @@ class UserCreateForm(UserCreationForm):
 
 class UserUpdateForm(UserCreateForm):
     def clean_username(self):
-        username = self.cleaned_data.get("username")
-        return username
+        new_username = self.cleaned_data.get("username")
+        old_username = self.get_initial_for_field(
+            self.fields['username'], 'username'
+        )
+
+        if new_username.lower() != old_username.lower():
+            if (
+                    new_username
+                    and
+                    self._meta.model.objects.filter(
+                        username__iexact=new_username
+                    ).exists()
+            ):
+                self._update_errors(
+                    ValidationError(
+                        {
+                            "username": self.instance.unique_error_message(
+                                self._meta.model, ["username"]
+                            )
+                        }
+                    )
+                )
+            else:
+                return new_username
+        else:
+            return new_username
