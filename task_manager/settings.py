e31d2b59ebd2cad1483ba77127044bf6be2b4e25
Sergey R <goodstop687@gmail.com>
2024-08-23 19:52:45 +0300

    configure allowed hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -32,7 +32,13 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = [] + os.getenv('ALLOWED_HOSTS', default='').split()
+ALLOWED_HOSTS = [
+    'webserver',
+    '127.0.0.1',
+    '0.0.0.0',
+    'testserver',
+    'localhost',
+]
 
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
