3494672ecb757145f6e80f3c323a37ad65256a00
Sergey R <goodstop687@gmail.com>
2024-09-13 11:57:52 +0300

    refactor users app tests

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -1,6 +1,6 @@
 """test module for user login/logout"""
 
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from http import HTTPStatus
